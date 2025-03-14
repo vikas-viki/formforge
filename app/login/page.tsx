3bab57125695f693210865adf3bb074ea92adbee
vikas-viki <viki57394@gmail.com>
2025-03-14 21:07:09 +0530

    added a smooth transitioned signup tab

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,10 +11,10 @@ const Login = () => {
     const switchToLogin = () => setActiveTab("login");
 
     return (
-        <div className={`w-[100vw] h-[100vh] bg-green-400 font-outfit p-10 flex justify-center items-center select-none ${outfit.className}`}>
+        <div className={`w-[100vw] h-[100vh]  bg-green-400 font-outfit p-10 flex justify-center items-start pt-[10%] select-none ${outfit.className}`}>
             <data className="flex gap-10 p-10 bg-green-600 h-max w-max rounded-[10px]">
                 <div className="flex flex-col gap-2 justify-start items-start max-w-[300px]">
-                    <span className="text-[30px] font-bold">
+                    <span className="text-[30px] font-bold brightness-60">
                         Get your certificates approved at one place.
                     </span>
                 </div>
@@ -42,8 +42,8 @@ const Login = () => {
                         </span>
                     </div>
                     <div className="flex flex-col gap-6 center">
-                        <div className="flex flex-col gap-1 ">
-                            <label htmlFor="email" className="text-[18px]">Email</label>
+                        <div className="flex flex-col ">
+                            <label htmlFor="email" className="text-[18px] pl-1">Email</label>
                             <input
                                 type="email"
                                 required
@@ -52,8 +52,8 @@ const Login = () => {
                                 className="outline-none border-[1px] rounded-[10px] px-4 py-2 text-[17.4px]"
                             />
                         </div>
-                        <div className="flex flex-col gap-1">
-                            <label htmlFor="password" className="text-[18px]">Password</label>
+                        <div className="flex flex-col">
+                            <label htmlFor="password" className="text-[18px] pl-1">Password</label>
                             <input
                                 type="password"
                                 required
@@ -63,12 +63,23 @@ const Login = () => {
                                 className="outline-none border-[1px] rounded-[10px] px-4 py-2 text-[17.4px]"
                             />
                         </div>
-                        <div className="flex justify-start items-center gap-2 pl-1">
-                            <input type="checkbox" id="remember" className="w-[15px] h-[15px] accent-green-700" />
-                            <label className="text-[17px] text-slate-800 cursor-pointer" htmlFor="remember">remember me</label>
+                        <div className={`flex flex-col transition-all duration-400 overflow-hidden ${activeTab === "login" ? "max-h-[0px] -my-4" : "max-h-[100px] my-0"}`}>
+                            <label htmlFor="cPassword" className="text-[18px] pl-1">Confirm Password</label>
+                            <input
+                                type="password"
+                                required
+                                id="cPassword"
+                                placeholder="********"
+                                minLength={8}
+                                className="outline-none border-[1px] rounded-[10px] px-4 py-2 text-[17.4px]"
+                            />
+                        </div>
+                        <div className={`flex justify-start opacity-[0.65] items-center gap-2 pl-1`}>
+                            <input type="checkbox" id="remember" className="w-[15px] h-[15px] accent-green-700 " />
+                            <label className="text-[17px] cursor-pointer" htmlFor="remember">remember me</label>
                         </div>
                         <div>
-                            <button className="border w-full px-4 py-3 text-[17px] bg-green-700 transition-all duration-300 cursor-pointer hover:bg-green-800 text-white rounded-[10px]">Login</button>
+                            <button className="border w-full px-4 mt-4 py-3 text-[17px] bg-green-700 transition-all duration-300 cursor-pointer hover:bg-green-800 text-white rounded-[10px]">Login</button>
                         </div>
                     </div>
                 </div>
