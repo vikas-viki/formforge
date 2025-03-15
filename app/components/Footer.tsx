3f67f3ff257d7d8115c9b0342f0791935bd9b0a2
vikas-viki <viki57394@gmail.com>
2025-03-15 22:02:24 +0530

    created footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,29 +1,69 @@
+import { Earth, Facebook, Instagram, Linkedin, LocateIcon, Mail, MapPin, Phone, Youtube } from "lucide-react";
+import Link from "next/link";
+import { quickSand, roboto } from "../library/font";
+
 const Footer = () => {
 
-    const blocks = [
-        {
-            title: "College Helpline",
-            content: "<span>Phone: 1234567890</span><span>Landline: 1234567890</span>"
-        }
-    ];
-
     return (
-        <div className="w-full h-[100vh] bg-green-700 relative">
-            <div className="absolute top-[-40px] " style={{ clipPath: "polygon(0 50%, 100% 0, 100% 100%, 0 100%)" }}></div>
-            <div className="flex items-start justify-center">
-                {
-                    blocks.map((block, i) => (
-                        <div key={i} className="w-full  flex flex-col group justify-center items-start px-[10%] bg-green-500 py-[30px]  border">
-                            <div className="flex items-center justify-between w-[50%]">
-                                <h1 className="font-outfit font-bold text-[28px]">{block.title}</h1>
-                                <button className="w-[50px] h-[30px] font-bold text-[28px] cursor-pointer">+</button>
+        <div
+            className='relative h-[390px]'
+            style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
+        >
+            <div className='relative h-[calc(100vh+390px)] -top-[100vh]'>
+                <div className='h-[390px] sticky top-[calc(100vh-390px)]'>
+                    <div className="w-full h-max bg-green-700">
+                        <div className="w-full h-max bg-green-700 relative flex gap-10 p-10 justify-between items-center">
+                            <div className="flex flex-col justify-center items-start gap-7 p-5 w-[50%]">
+                                <span className="font-bold text-[20px] block">FormForge</span>
+                                <p className="w-[70%] text-[17px]">Streamline form submissions, simplify approvals, and ensure seamless communication between students, teachers, and admins with real-time updates.</p>
+                                <div className="flex gap-5 justify-start items-start">
+                                    <Link href="https://www.facebook.com/groups/12016654077" target="_blank">
+                                        <span className="block rounded-[50%] bg-green-500 cursor-pointer p-2">
+                                            <Facebook size={23} />
+                                        </span>
+                                    </Link>
+                                    <Link href="https://www.linkedin.com/school/bhandarkars-arts-science-college/" target="_blank">
+                                        <span className="block rounded-[50%] bg-green-500 cursor-pointer p-2">
+                                            <Linkedin size={23} />
+                                        </span>
+                                    </Link>
+                                    <Link href="https://www.youtube.com/@bhandarkarsartsandsciencec8968" target="_blank">
+                                        <span className="block rounded-[50%] bg-green-500 cursor-pointer p-2">
+                                            <Youtube size={23} />
+                                        </span>
+                                    </Link>
+                                    <Link href="https://www.instagram.com/bhandakars_college_kundapura" target="_blank">
+                                        <span className="block rounded-[50%] bg-green-500 cursor-pointer p-2">
+                                            <Instagram size={23} />
+                                        </span>
+                                    </Link>
+                                    <Link href="https://www.basck.in" target="_blank" >
+                                        <span className="block rounded-[50%] bg-green-500 cursor-pointer p-2">
+                                            <Earth size={23} />
+                                        </span>
+                                    </Link>
+                                </div>
                             </div>
-                            <div className="transition-all duration-300  absolute h-max top-[10px]">
-                                {block.content}
+                            <div className="flex flex-col gap-5 p-5 w-[50%] justify-start self-start">
+                                <span className="block font-bold text-[20px]">Contact</span>
+                                <div className="flex flex-col gap-4">
+                                    <span className="flex gap-2">
+                                        <Phone /> (+91) 1234567890
+                                    </span>
+                                    <span className="flex gap-2">
+                                        <Mail /> contact@basck.in
+                                    </span>
+                                    <span className="flex gap-2">
+                                        <MapPin /> 123, Lorem Ipsum, Dolor Sit, Amet
+                                    </span>
+                                </div>
                             </div>
                         </div>
-                    ))
-                }
+                        <div className={`w-full py-6  bg-green-800 text-center ${quickSand.className}`}>
+                            <span className="block text-[17px] font-medium">© 2025 FormForge. All rights reserved | An initiative by Dept of CS, BASCK</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- app/library/font.ts
+++ app/library/font.ts
@@ -1,6 +1,16 @@
-import { Outfit } from "next/font/google";
+import { Outfit, Quicksand, Roboto } from "next/font/google";
 
 export const outfit = Outfit({
     weight: ["400", "500", "600", "700", "800", "900"],
     variable: "--font-outfit",
+})
+
+export const roboto = Roboto({
+    weight: ["400", "500", "600", "700", "800", "900"],
+    variable: "--font-roboto",
+})
+
+export const quickSand = Quicksand({
+    weight: ["400", "500", "600", "700"],
+    variable: "--font-quicksand",
 })
