a131fac4478c83b9aff88e2b6e276428c82e9388
vikas-viki <viki57394@gmail.com>
2025-03-30 18:02:54 +0530

    workflow section responsice

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -2,34 +2,38 @@ import { ArrowUpRight } from "lucide-react";
 import { outfit } from "../library/font";
 
 export default function Features() {
+    const infoClasses = "text-[12px] md:text-[16px] opacity-60";
+    const headingClasses = "text-[20px] md:text-[24px] text-[#D4D6F5]";
+    const descriptionClasses = "text-[17px] md:text-[20px] text-[#B6B7C5]";
+
     return (
-        <section id="features" className={`w-[100vw] h-max overflow-hidden p-20 ${outfit.className} relative flex flex-col justfiy-center items-center`}>
-            <span className="text-[#AEB3F8] self-start block pl-[5%] text-[24px]">Features</span>
-            <div className="  h-full bg-[#161624] relative rounded-[15px] w-[90%] border-[0.6px] outline-none mt-10 relative overflow-hidden">
-                <div className="features-bg p-10 pl-0 pt-0 w-full h-full mt-[40px] ml-[40px]">
-                    <div className="flex py-5 pt-0 gap-10 pr-10">
-                        <div className="flex flex-col gap-4 z-2 bg-[#161624] features-head p-4">
-                            <span className="font-medium text-[30px] text-[#D1D5DB]">Why Choose Applify?</span>
-                            <span className="text-[22px] text-[#D2D4F5] leading-[26px]">Streamline the application process and verification in a single, centralized platform.</span>
-                            <button className="cursor-pointer flex gap-2 bg-[#6D99F1] py-2 w-max text-[15px] px-3 mt-3 justify-center items-center">Get Started<ArrowUpRight size={15} /></button>
+        <section id="features" className={`w-[100vw] h-max overflow-hidden p-10 lg:p-20 ${outfit.className} relative flex flex-col justfiy-center items-center`}>
+            <span className="text-[#AEB3F8] w-full self-start block pl-[5%] text-[20px] md:text-[24px]">Features</span>
+            <div className="  h-full bg-[#161624] relative rounded-[15px] w-full lg:w-[90%] border-[0.6px] outline-none mt-10 relative overflow-hidden">
+                <div className="features-bg  pl-0 pt-0 w-full h-full lg:mt-[40px] lg:ml-[40px]">
+                    <div className="flex flex-col lg:flex-row py-5 pt-0 gap-10 lg:pr-10">
+                        <div className="flex flex-col gap-4 z-2 bg-[#161624] features-head p-8 sm:p-10 lg:p-4 ">
+                            <span className="font-medium text-[25px] md:text-[30px] text-[#D1D5DB]">Why Choose Applify?</span>
+                            <span className="text-[18px] md:text-[22px] text-[#D2D4F5] leading-[26px]">Streamline the application process and verification in a single, centralized platform.</span>
+                            <button className="cursor-pointer flex gap-2 bg-[#6D99F1] py-2 w-max text-[12px] md:text-[15px] px-3 mt-3 justify-center items-center">Get Started<ArrowUpRight size={15} /></button>
                         </div>
-                        <div className="flex flex-col gap-3 p-10 rounded-[20px] bg-[#12121C] border-[0.6px] mt-10">
-                            <span className="text-[16px] opacity-60">ALL IN ONE</span>
-                            <span className="text-[24px] text-[#D4D6F5]">Multi-Form Support</span>
-                            <span className="text-[20px] text-[#B6B7C5]">Access a wide range of college applications, including Study Certificates, Transfer Certificates and Mid-day Meals.. all in one place.</span>
+                        <div className="flex flex-col gap-3 mx-5 md:mx-10 lg:mx-0 p-5 sm:p-10 rounded-[20px] bg-[#12121C] border-[0.6px] mt-0 md:mt-10 lg:mr-10 lg:ml-0 ">
+                            <span className={infoClasses}>ALL IN ONE</span>
+                            <span className={headingClasses}>Multi-Form Support</span>
+                            <span className={descriptionClasses}>Access a wide range of college applications, including Study Certificates, Transfer Certificates and Mid-day Meals.. all in one place.</span>
                         </div>
                     </div>
-                    <div className="flex pt-5 gap-10 ml-10 pr-10">
-                        <div className="flex flex-col gap-3 p-10 rounded-[20px] bg-[#12121C] border-[0.6px]">
-                            <span className="text-[16px] opacity-60">BUILT IN SECURITY</span>
-                            <span className="text-[24px] text-[#D4D6F5]">Secure & Private</span>
-                            <span className="text-[20px] text-[#B6B7C5]">Your applications, personal information and approval controls are securely protected with advanced encryptions.</span>
+                    <div className="p-5 md:p-10 flex flex-col lg:flex-row pt-5 gap-10 lg:mr-10 lg:pr-10">
+                        <div className="flex flex-col gap-3 p-5 sm:p-10 rounded-[20px] bg-[#12121C] border-[0.6px]">
+                            <span className={infoClasses}>BUILT IN SECURITY</span>
+                            <span className={headingClasses}>Secure & Private</span>
+                            <span className={descriptionClasses}>Your applications, personal information and approval controls are securely protected with advanced encryptions.</span>
                         </div>
 
-                        <div className="flex flex-col gap-3 p-10 rounded-[20px] bg-[#12121C] border-[0.6px] min-w-[45%]">
-                            <span className="text-[16px] opacity-60">ON - TRACK</span>
-                            <span className="text-[24px] text-[#D4D6F5]">Seamless Tracking</span>
-                            <span className="text-[20px] text-[#B6B7C5]">Stay informed about your application status in real-time with instant updates.</span>
+                        <div className="flex flex-col gap-3 p-5 sm:p-10 rounded-[20px] bg-[#12121C] border-[0.6px] min-w-[45%]">
+                            <span className={infoClasses}>ON - TRACK</span>
+                            <span className={headingClasses}>Seamless Tracking</span>
+                            <span className={descriptionClasses}>Stay informed about your application status in real-time with instant updates.</span>
                         </div>
                     </div>
                 </div>
--- app/components/Workflow.tsx
+++ app/components/Workflow.tsx
@@ -1,35 +1,39 @@
 import { outfit } from "../library/font";
 
 export default function Workflow() {
+
+    const infoClasses = "font-bold text-[#B0B0F2] text-[40px] md:text-[45px] lg:text-[50px] opacity-70 leading-[65px]";
+    const headingClasses = "font-medium text-[22px] md:text-[26px] text-[#D4D6F5]";
+    const descriptionClasses = "font-medium text-[17px] md:text-[20px] text-[#B6B7C5]";
+
     return (
         <div id="workflow" className={`flex flex-col justify-center items-center ${outfit.className} mt-25`}>
             <div className="flex flex-col gap-2 items-center mb-20">
-                <span className="text-[#9CA0DE] text-[22px]">Step by Step</span>
-                <span className="text-[#D4D6F5] text-[35px]">How Does Applify Work</span>
+                <span className="text-[#9CA0DE] text-[18px] text-center md:text-[22px]">Step by Step</span>
+                <span className="text-[#D4D6F5] text-[30px] text-center md:text-[35px] px-5 ">How Does Applify Work</span>
             </div>
-            <div className="w-[2px] bg-white h-full"></div>
-            <div className="flex gap-20">
-                <div className="flex flex-col gap-2 px-8 py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
-                    <span className="font-bold text-[#B0B0F2] text-[50px] opacity-70 leading-[65px]">01</span>
-                    <span className="font-medium text-[26px] text-[#D4D6F5]">Sign-up</span>
-                    <span className="font-medium text-[20px] text-[#B6B7C5]">Fill in the required details and complete your registration to get started.</span>
+            <div className="flex gap-10 md:gap-20 flex-wrap justify-center items-center px-10 md:p-5">
+                <div className="flex flex-col gap-2 px-6 sm:px-8 py-4 sm:py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
+                    <span className={infoClasses}>01</span>
+                    <span className={headingClasses}>Sign-up</span>
+                    <span className={descriptionClasses}>Fill in the required details and complete your registration to get started.</span>
                 </div>
-                <div className="flex flex-col gap-2 px-8 py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
-                    <span className="font-bold text-[#B0B0F2] text-[50px] opacity-70 leading-[65px]">02</span>
-                    <span className="font-medium text-[26px] text-[#D4D6F5]">Application submission</span>
-                    <span className="font-medium text-[20px] text-[#B6B7C5]">Fill out the required application with details and submit it.</span>
+                <div className="flex flex-col gap-2 px-6 sm:px-8 py-4 sm:py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
+                    <span className={infoClasses}>02</span>
+                    <span className={headingClasses}>Application submission</span>
+                    <span className={descriptionClasses}>Fill out the required application with details and submit it.</span>
                 </div>
             </div>
-            <div className="flex gap-20 mt-20 ml-25">
-                <div className="flex flex-col gap-2 px-8 py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
-                    <span className="font-bold text-[#B0B0F2] text-[50px] opacity-70 leading-[65px]">03</span>
-                    <span className="font-medium text-[26px] text-[#D4D6F5]">Admin Approval</span>
-                    <span className="font-medium text-[20px] text-[#B6B7C5]">Admin verifies the application to ensure its validity and intent.</span>
+            <div className="flex gap-10 md:gap-20 mt-10 md:mt-20 ml-0 xl:ml-25 flex-wrap justify-center items-center px-10 md:p-5">
+                <div className="flex flex-col gap-2 px-6 sm:px-8 py-4 sm:py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
+                    <span className={infoClasses}>03</span>
+                    <span className={headingClasses}>Admin Approval</span>
+                    <span className={descriptionClasses}>Admin verifies the application to ensure its validity and intent.</span>
                 </div>
-                <div className="flex flex-col gap-2 px-8 py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
-                    <span className="font-bold text-[#B0B0F2] text-[50px] opacity-70 leading-[65px]">04</span>
-                    <span className="font-medium text-[26px] text-[#D4D6F5]">Certificate Generation</span>
-                    <span className="font-medium text-[20px] text-[#B6B7C5]">Once approved by the admin, download your certificate anytime, anywhere.</span>
+                <div className="flex flex-col gap-2 px-6 sm:px-8 py-4 sm:py-6 rounded-[15px] border-[0.5px] bg-[#12121A] max-w-[450px]">
+                    <span className={infoClasses}>04</span>
+                    <span className={headingClasses}>Certificate Generation</span>
+                    <span className={descriptionClasses}>Once approved by the admin, download your certificate anytime, anywhere.</span>
                 </div>
             </div>
         </div>
